6b752101da4790265805347dd1cd69da97a311db
Joshua Morony <joshua.morony@gmail.com>
2021-10-26 12:01:43 +1030

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-angular-web3-login',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
